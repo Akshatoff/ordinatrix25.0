6e162fc104723979d6abeff62e90bd47a8db8a2a
Akshatoff <akshatascoder@gmail.com>
2025-03-25 17:11:55 +0530

    Popup update

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,12 +2,30 @@ import { useLayoutEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SpotlightCard from "./SpotlightCard";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "black",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 gsap.registerPlugin(ScrollTrigger);
 
 const AnimatedCards = () => {
   const containerRef = useRef(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  let [isOpen, setIsOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
   const cards_content = [
     {
@@ -16,7 +34,7 @@ const AnimatedCards = () => {
     },
     {
       title: "Snap@trix",
-      text: "A thrilling challenge that includes both online prelims and offline finals, testing participants' ability to create compelling visual narratives.",
+      text: "A thrilling challenge to test participants' ability to create compelling visual narratives.",
     },
     {
       title: "Game@trix",
@@ -40,20 +58,50 @@ const AnimatedCards = () => {
     },
     {
       title: "Drone@trix",
-      text: "Obstacle course Track using cones, hoops, tunnels, or cardboard structures. Use obstacles that challenge different skills:",
+      text: "Obstacle course Track using cones, hoops, tunnels, or cardboard structures",
+    },
+  ];
+
+  const pop_up = [
+    {
+      title: "Innovate@trix",
+      class: "9 - 12th",
+      participants: "4",
+      teams: "1 team per school",
+      mode: "Prelims - Submission based, Finals - Offline",
+      text: "Teams will be challenged to reimagine an established brand, merging innovative design with technical execution to create a unique and practical solution. Participants will collaborate to conceptualize, build, and present a project that is both visually impactful and functionally effective, meeting all the given requirements.",
+    },
+    {
+      title: "Snap@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
+    },
+    {
+      title: "Game@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
+    },
+    {
+      title: "Movie@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
+    },
+    {
+      title: "Byte@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
+    },
+    {
+      title: "Crypt@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
+    },
+    {
+      title: "Code@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
+    },
+    {
+      title: "Drone@trix",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quae quisquam. Debitis dolorem maiores pariatur reprehenderit mollitia expedita saepe eius voluptate! Debitis ut, obcaecati autem qui impedit officia aliquid deleniti adipisci in, deserunt aspernatur totam natus quaerat cupiditate error commodi.",
     },
   ];
 
   useLayoutEffect(() => {
-    // Remove the local Lenis instance - we already have one in the App component
-    // const lenis = new Lenis({
-    //   smooth: true,
-    // });
-
-    // lenis.on("scroll", ScrollTrigger.update);
-    // gsap.ticker.add((time) => lenis.raf(time * 1000));
-    // gsap.ticker.lagSmoothing(0);
-
     const ctx = gsap.context(() => {
       const cards = containerRef.current.querySelectorAll(
         ".card:not(:first-child)"
@@ -119,9 +167,40 @@ const AnimatedCards = () => {
             <h2 className="text" id="card-des">
               {card.text}
             </h2>
+            <button
+              className="link"
+              onClick={() => {
+                setSelectedIndex(index);
+                setIsOpen(true);
+              }}
+            >
+              Learn More
+            </button>
           </SpotlightCard>
         ))}
       </div>
+      <Modal
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {selectedIndex !== null && (
+            <>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                {pop_up[selectedIndex].title}
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {pop_up[selectedIndex].text}
+              </Typography>
+            </>
+          )}
+          <button onClick={() => setIsOpen(false)} className="btn btn-close">
+            Close
+          </button>
+        </Box>
+      </Modal>
     </div>
   );
 };
